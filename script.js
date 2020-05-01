7237b0ee9bc3d55019cdeb7d33ba5d117f83a282
SilviHambardzumyan <62055378+SilviHambardzumyan@users.noreply.github.com>
2020-05-01 18:33:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var logpass =  [
+  { 
+    email: "silvihambardzumyan1999@gmail.com",
+    lock: "123456"
+  }
+]
+
+function getInfo() {
+  var email = document.getElementById("email").value
+  var lock = document.getElementById("lock").value
+
+  console.log("correct")
+}
