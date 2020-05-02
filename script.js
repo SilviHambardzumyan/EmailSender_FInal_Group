4ee264fff146a381e059c72e38c3c792588266f2
annaasia <62056048+annaasia@users.noreply.github.com>
2020-05-02 13:42:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -200,3 +200,23 @@ function ChooseFromTheListBelow() {
         divContainer.innerHTML = "";
         divContainer.appendChild(table);
     }
+
+
+
+Anna 
+function SubjectCharacters (inputtxt, minlength, maxlength) {
+  var field = inputtxt.value;
+  var mnlen = minlength;
+  var mxlen = maxlength;
+
+  if(field.length <mnlen || field> mxlen.length) { 
+    document.getElementById("subjectValid").innerHTML = "We suggest that you keep your subject between " +mnlen+ " and " +mxlen+ " characters";
+  } else { 
+    document.getElementById("subjectValid").innerHTML = "Looks good!";
+  }
+}
+
+function sendfunction() {
+  console.log("Your email has been sent");
+}
+
